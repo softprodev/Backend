6b65654f40f7c91be7d98c009b5dbf9fabcb1bfd
SoftProDev <softprodev15@gmail.com>
2018-10-22 15:58:46 +0200

    PatrykMilewski suggestions

--- Manager/src/main/java/io/raspberrywallet/manager/common/interfaces/Destroyable.java
+++
@@ -1,7 +0,0 @@
-package io.raspberrywallet.manager.common.interfaces;
-
-public interface Destroyable {
-
-     void destroy();
-
-}
--- Manager/src/main/java/io/raspberrywallet/manager/common/wrappers/ByteWrapper.java
+++ Manager/src/main/java/io/raspberrywallet/manager/common/wrappers/ByteWrapper.java
@@ -1,19 +1,18 @@
 package io.raspberrywallet.manager.common.wrappers;
 
-import io.raspberrywallet.manager.common.ArrayDestroyer;
-import io.raspberrywallet.manager.common.interfaces.Destroyable;
-import lombok.Getter;
-
 import java.io.Serializable;
 import java.util.Arrays;
 
-public class ByteWrapper implements Serializable, Destroyable {
+public class ByteWrapper implements Serializable {
     
-    @Getter
     private byte[] data;
     
     public ByteWrapper(byte[] data) {
         this.data = data;
+    }
+    
+    public byte[] getData() {
+        return data;
     }
     
     @Override
@@ -32,10 +31,4 @@ public class ByteWrapper implements Serializable, Destroyable {
         ByteWrapper otherCasted = (ByteWrapper)other;
         return this.hashCode() == otherCasted.hashCode();
     }
-    
-    @Override
-    public void destroy() {
-        ArrayDestroyer.destroy(data);
-    }
-
 }
